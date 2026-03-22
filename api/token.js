f5ee1ca2dd4d47600d8ba6249bcdfa8bdff40cef
BANANACATHUBV2 <maior0628@gmail.com>
2026-03-21 22:25:37 -0300

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 
 export default function handler(req, res) {
-  const periodo = Math.floor(Date.now() / 600000)
+  const periodo = Math.floor(Date.now() / 30000)
   const token = crypto
     .createHmac('sha256', process.env.TOKEN_SECRET)
     .update(String(periodo))
